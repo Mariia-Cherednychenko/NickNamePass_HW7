cf129df22ac9e872567bf4c3b1f198e051cd5ac0
Mariia Cherednychenko <Mary0901@ukr.net>
2020-12-16 07:46:30 +0200

    if only small letters should be in nickname and username, and only digits - in pass

--- src/ua/ithlillel/dnipro/Cherednychenko/Main.java
+++ src/ua/ithlillel/dnipro/Cherednychenko/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         String textWithUsers = "programmer:vasia@123,programmer:vasia,vasia@1234,vasia,vasia:@trulala";
-        String regex = "^(?:([a-z]*?):)?([a-z]+)@?(\\d+)?$";
+        String regex = "^(?:([a-z]+):)?([a-z]+)@?(\\d+)?$";
 
         UserParser userParser = new UserParser(regex);
         userParser.showList(userParser.parseList(textWithUsers));
